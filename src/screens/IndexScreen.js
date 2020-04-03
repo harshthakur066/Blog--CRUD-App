bcdec5eb9a59dc441f734b32e29225c6339bef4b
harshthakur066 <harshthakur066@gmail.com>
2020-04-03 19:50:06 +0530

    app setup and navigation

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <>
+      <Text>Index View</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
