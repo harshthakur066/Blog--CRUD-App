0010a7e4968b812b841913bbaf54a360060781a7
harshthakur066 <harshthakur066@gmail.com>
2020-04-04 15:33:20 +0530

    addBlogPost using useStatw

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { Text, StyleSheet, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
+import { FlatList } from "react-native-gesture-handler";
 
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
   return (
     <>
       <Text>Index View</Text>
+      <Button title="Add Blog" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text> {item.title} </Text>;
+        }}
+      />
     </>
   );
 };
