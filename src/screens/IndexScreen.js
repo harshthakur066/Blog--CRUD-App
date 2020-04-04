d7dee315c77bc28dc8c7fb42ad092c2c67ac59fb
harshthakur066 <harshthakur066@gmail.com>
2020-04-05 01:36:27 +0530

    set up for automated contex

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,16 @@
 import React, { useContext } from "react";
 import { Text, StyleSheet, Button } from "react-native";
-import BlogContext from "../context/BlogContext";
+import { Context as BlogContext } from "../context/BlogContext";
 import { FlatList } from "react-native-gesture-handler";
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(BlogContext);
   return (
     <>
       <Text>Index View</Text>
       <Button title="Add Blog" onPress={addBlogPost} />
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return <Text> {item.title} </Text>;
